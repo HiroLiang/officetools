1a0a4ebbafe863f1f0aded2a0c0a473be4d52f49
2301031 <2301031@systex.com.tw>
2024-02-23 11:09:18 +0800

    #9 - add handler , by Hiro

---
+++ src/main/java/com/hl/officetools/task/word/WordTaskHandler.java
@@ -0,0 +1,14 @@
+package com.hl.officetools.task.word;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+public class WordTaskHandler {
+	
+	private XWPFDocument document;
+	
+	public WordTaskHandler render() {
+		
+		return this;
+	}
+
+}
